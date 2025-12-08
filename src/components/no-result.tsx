98e312f970b638bf394e7d1e4f33bba69966d916
skylinkvpn <phyo.w28@gmail.com>
2025-12-09 04:50:01 +0630

    Change default message to Burmese 'ရလဒ်မတွေ့ပါ'

--- src/components/no-result.tsx
+++ src/components/no-result.tsx
@@ -4,7 +4,7 @@ interface Props {
     message?: string;
 }
 
-export default function NoResult({ message = "No Result" }: Props) {
+export default function NoResult({ message = "ရလဒ်မတွေ့ပါ" }: Props) {
     return (
         <div className="grid gap-2 place-items-center">
             <span className="text-2xl">¯\_(ツ)_/¯</span>
