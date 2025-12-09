2118fdae9c3698230167d15284938a92a6dc2a70
skylinkvpn <phyo.w28@gmail.com>
2025-12-10 04:31:14 +0630

    Update page.tsx

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -4,6 +4,9 @@ import { createPageTitle } from "@/src/core/utils";
 import { getTags } from "@/src/core/actions/tags";
 import TagsList from "@/src/components/tags-list";
 
+// ✅ build time မှာ static generate မလုပ်ဘဲ runtime မှာပဲ DB ခေါ်အောင်
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
     title: createPageTitle("Tags")
 };
